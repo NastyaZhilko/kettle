2e427a4cc6fc1bf0c5b8853d89c2b6c9980593ac
Anastasiya Zhilko <nasten4ik2010@gmail.com>
2024-01-22 18:06:00 +0300

    added styles

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ const MIN_WATER_VOLUME = 0;
 const STEP_WATER_VOLUME = 0.25;
 const INITIAL_TEMPERATURE = 0;
 const INITIAL_KETTLE_STATE = 'off';
-type Switch = Partial<KettleStateType>;
 
 const SLIDER_VALUES = [MIN_WATER_VOLUME, 0.25, 0.5, 0.75, 1] as const;
 const SLIDER_LABELS: Record<SliderValue, number> = {
@@ -25,16 +24,15 @@ type SliderValue = (typeof SLIDER_VALUES)[number];
 
 function App() {
 
-    const toggleButtons: SwitchButtonProps<Switch>[] = [
+    const toggleButtons: SwitchButtonProps<KettleStateType>[] = [
         {
             value: 'on',
             label: 'On',
         },
         {
-            value: 'stopped',
+            value: 'off',
             label: 'Off',
         },
-
     ];
 
     const sliderMarks: SliderMark[] = SLIDER_VALUES.map((value) => ({
@@ -46,15 +44,16 @@ function App() {
     const [kettleState, setKettleState] = useState<KettleStateType>(INITIAL_KETTLE_STATE)
     const [temperature, setTemperature] = useState<number>(INITIAL_TEMPERATURE)
     const [message, setMessage] = useState<string>('The kettle is off')
-    console.log(kettleState)
-
-    const [switchState, setSwitchState] = useState<Switch>(INITIAL_KETTLE_STATE);
 
     useEffect(() => {
         if (kettleState === 'on') {
             const idTimeout = setTimeout(() => {
                 setMessage('The kettle is boiled')
                 setKettleState('boiled')
+                setTimeout(() => {
+                    setMessage('The kettle is off')
+                    setKettleState('off')
+                }, 1000)
             }, 10000)
             return () => {
                 clearTimeout(idTimeout)
@@ -64,7 +63,6 @@ function App() {
 
     useEffect(() => {
         if (kettleState === 'on') {
-            console.log(temperature)
             const idInterval = setInterval(() => {
                 setTemperature((currentTemperature) => currentTemperature + 10)
 
@@ -74,50 +72,49 @@ function App() {
             }
         }
     }, [kettleState])
-    console.log(temperature)
 
     const handleSliderChange = (value: number | number[]) => {
         setWaterVolume(value as SliderValue);
     }
 
-    const toggleSwitch = (state: Switch) => {
-        console.log(kettleState)
-        console.log(state)
+    const toggleSwitch = (state: KettleStateType) => {
         setKettleState(state)
-        setMessage(`The kettle is ${state}`)
-    }
-
-    const handleClickOn = () => {
-        setKettleState('on')
-        setMessage('Kettle is on')
-        setTemperature(0)
-    }
-    const handleClickOff = () => {
-        setKettleState('stopped')
-        setMessage('The kettle is stopped')
+        setMessage(`The kettle is stopped`)
+        if (state === 'on') {
+            setTemperature(0)
+            setMessage(`The kettle is ${state}`)
+        }
     }
     return (
-        <div className="App">
-            <div className={styles.waterVolume}>
-                <div className={styles.waterVolumeInformation}>
-                    Water volume: {waterVolume} liter
+        <div className={styles.root}>
+            <div className={styles.blockImage}>
+                <img src='/kettle.jpg' alt='kettle' className={styles.image}/>
+            </div>
+            <div className={styles.blockInfo}>
+                <div className={styles.waterVolume}>
+                    <div className={styles.waterVolumeInformation}>
+                        Water volume: {waterVolume} liter
+                    </div>
+                    <Slider
+                        minValue={MIN_WATER_VOLUME}
+                        maxValue={MAX_WATER_VOLUME}
+                        step={STEP_WATER_VOLUME}
+                        marks={sliderMarks}
+                        value={waterVolume}
+                        onChange={handleSliderChange}
+                    />
                 </div>
-                <Slider
-                    minValue={MIN_WATER_VOLUME}
-                    maxValue={MAX_WATER_VOLUME}
-                    step={STEP_WATER_VOLUME}
-                    marks={sliderMarks}
-                    value={waterVolume}
-                    onChange={handleSliderChange}
-                />
+                <div>
+                    <SwitchButtons<KettleStateType>
+                        buttons={toggleButtons}
+                        value={kettleState}
+                        onChange={toggleSwitch}
+                        disabled={!waterVolume}
+                    />
+                </div>
+                <div>Water temperature: {temperature} ℃</div>
+                <div>{message}</div>
             </div>
-            <div>
-                <button onClick={handleClickOn} disabled={kettleState === 'on'}>on</button>
-                <button onClick={handleClickOff} disabled={kettleState !== 'on'}>off</button>
-            </div>
-            <div><SwitchButtons<Switch> buttons={toggleButtons} value={kettleState} onChange={toggleSwitch}/></div>
-            <div>Water temperature: {temperature} ℃</div>
-            <div>{message}</div>
         </div>
     );
 }
--- src/components/SwitchButtons/SwitchButtons.tsx
+++ src/components/SwitchButtons/SwitchButtons.tsx
@@ -11,24 +11,30 @@ type Props<T extends string> = {
     buttons: SwitchButton<T>[];
     value: T;
     onChange(value: T): void;
+    disabled?: boolean
 };
 
 export function SwitchButtons<T extends string>({
                                                     buttons,
                                                     value,
                                                     onChange,
+                                                    disabled
                                                 }: Props<T>) {
     return (
         <div className={styles.root}>
-            {buttons.map(({ label, value: buttonValue }) => (
-                <button
-                    key={buttonValue}
-                    onClick={() => onChange(buttonValue)}
-                    className={`${styles.button} ${value === buttonValue && styles.active}`}
-                >
-                    {label}
-                </button>
-            ))}
+            {buttons.map(({ label, value: buttonValue }) => {
+                const isActiveButton = value===buttonValue
+                return (
+                    <button
+                        key={buttonValue}
+                        onClick={() => onChange(buttonValue)}
+                        className={`${styles.button} ${isActiveButton && styles.active}`}
+                        disabled={isActiveButton || disabled}
+                    >
+                        {label}
+                    </button>
+                )
+            })}
         </div>
     );
 }
